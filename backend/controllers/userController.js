cdd68a3b2de1528951e5c100c9bf54374f93605d
EvanRPavone <EvanRPavone@gmail.com>
2024-01-19 13:29:58 -0500

    sign users up

---
+++ backend/controllers/userController.js
@@ -0,0 +1,30 @@
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d'}); // user will remain logged in for 3 days and then expire
+}
+
+// login user
+const loginUser = async (req, res) => {
+  res.json({ msg: 'login user' })
+}
+
+// signup user
+const signupUser = async (req, res) => {
+  const {email, firstName, lastName, password} = req.body
+  try {
+    const user = await User.signup(email, firstName, lastName, password)
+
+    //create a token
+    // HEADER.PAYLOAD.SIGNATURE
+    const token = createToken(user._id)
+
+    res.status(200).json({ email, token })
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+
+}
+
+module.exports = { loginUser, signupUser };
